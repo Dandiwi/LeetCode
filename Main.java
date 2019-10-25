37d5b5008836e8e560bbb9a3b1fea1047ed38218
Dandiwi <47329340+Dandiwi@users.noreply.github.com>
2019-10-25 11:56:13 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        char[]s = {'h','e','l','l','o'};
+        for (int i = 0; i<(s.length)/2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length -1 - i];
+            s[s.length- 1 -i] = temp;
+        }
+        System.out.println(s);
+    }
+}
